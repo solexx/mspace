76c010e235eee0fa581ea56186de271a777932a8
Jochen Spieker <js@wim.re>
2021-01-25 22:15:15 +0100

    Conversion to Python 3

--- benchmarks.py
+++ benchmarks.py
@@ -17,13 +17,13 @@ def build_tree(cls, objects, metric):
 
 def benchmark_k(tree, objects, min_dist, max_dist, step):
     mspace.dist_ctr = 0
-    print "#%s, size: %s, height: %s, nodes: %s" % (type(tree), len(tree),
-            tree.height, tree.num_nodes)
-    print "#doing %s searches" % len(objects)
-    print "#k\t%6s\t%8s\t%8s\t%8s" % (
-            "time", "distCt", "ratio", "resCt")
+    print("#%s, size: %s, height: %s, nodes: %s" % (type(tree), len(tree),
+            tree.height, tree.num_nodes))
+    print("#doing %s searches" % len(objects))
+    print("#k\t%6s\t%8s\t%8s\t%8s" % (
+            "time", "distCt", "ratio", "resCt"))
     for k in range(min_dist, max_dist+1, step):
-        print "%s\t" % k,
+        print("%s\t" % k, end=' ')
         start = time.time()
         num_results = 0
         for o in objects:
@@ -33,14 +33,14 @@ def benchmark_k(tree, objects, min_dist, max_dist, step):
         dist_per_object = float(mspace.dist_ctr) / len(objects)
         time_per_dist = time_per_object / dist_per_object
         res_per_object = float(num_results) / len(objects)
-        print "%6.4f\t%8.2f\t%0.6f\t%8.2f" % (
+        print("%6.4f\t%8.2f\t%0.6f\t%8.2f" % (
                 time_per_object, dist_per_object,
-                time_per_dist, res_per_object)
+                time_per_dist, res_per_object))
         mspace.dist_ctr = 0
 
 def benchmark_construction(cls, objects, metric, max, step):
-    print "#%s" % (cls)
-    print "#%8s\t%7s\t%12s\t%8s" % ("size", "height", "time", "per node")
+    print("#%s" % (cls))
+    print("#%8s\t%7s\t%12s\t%8s" % ("size", "height", "time", "per node"))
     for n in range(step, max+1, step):
         lst = objects[:n]
         start = time.time()
@@ -48,11 +48,11 @@ def benchmark_construction(cls, objects, metric, max, step):
         end = time.time()
         buildtime = float(end) - start
         time_per_node = buildtime / len(t)
-        print "%8s\t%7s\t%12.2f\t%8.6f" % (len(t), t.height, buildtime,
-                time_per_node)
+        print("%8s\t%7s\t%12.2f\t%8.6f" % (len(t), t.height, buildtime,
+                time_per_node))
         del t
         gc.collect()
-    print
+    print()
 
 
 def main():
@@ -64,7 +64,7 @@ def main():
     toSearch = get_sample(tokenizer(file), min(100, max(int(.01*num), 20)))
     gc.collect()
     for cls in [BKTree, VPTree]:
-        print
+        print()
         #benchmark_construction(cls, objects, levenshtein, num, int(num/6))
         tree = build_tree(cls, objects, levenshtein)
         benchmark_k(tree, toSearch, 0, 3, 1)
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 #""")
     try:
         main()
-    except KeyboardInterrupt, e:
+    except KeyboardInterrupt as e:
         sys.stdout.flush()
         sys.stderr.flush()
         sys.stderr.write("\n^C\n")
